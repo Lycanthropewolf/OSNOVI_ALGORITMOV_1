ca513cb28e6443fe1abc8bef62f051bef7b66b16
Антон <antonolegovich2017@icloud.com>
2023-01-08 18:06:50 +0600

    DZ

---
+++ OsnoviAlgoritm1Application.java
@@ -0,0 +1,13 @@
+package com.example.osnovi_algoritm_1______;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsnoviAlgoritm1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsnoviAlgoritm1Application.class, args);
+    }
+
+}
